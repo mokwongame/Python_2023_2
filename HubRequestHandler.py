599932386beaddb86e29daffda869c866e672c95
mokwongame <ictplusud@gmail.com>
2023-11-29 16:22:05 +0900

    sample_volt 웹 페이지 추가

--- HubRequestHandler.py
+++ HubRequestHandler.py
@@ -1,12 +1,15 @@
 from http.server import SimpleHTTPRequestHandler
 import time
+from urllib import parse
 
 class HubRequestHandler(SimpleHTTPRequestHandler): # SimpleHTTPRequestHandler를 상속받아 HubRequestHandler 클래스를 구현
     def do_GET(self):
         print(self.path)
-        if self.path == '/': self.writeHome() # 홈
-        elif self.path == '/meas_one_volt': self.writeMeasOneVolt()
-        else: self.writeNotFound() # 페이지가 없음
+        result = parse.urlsplit(self.path)
+        if result.path == '/': self.writeHome() # 홈
+        elif result.path == '/meas_one_volt': self.writeMeasOneVolt()
+        elif result.path == '/sample_volt': self.writeSampleVolt(result.query)
+        else: result.writeNotFound() # 페이지가 없음
     def writeHead(self, nStatus): # response의 header
         self.send_response(nStatus)
         self.send_header('content-type', 'text/html') # 속성(attribute), 값 순으로 입력
@@ -52,7 +55,28 @@ class HubRequestHandler(SimpleHTTPRequestHandler): # SimpleHTTPRequestHandler를
         html += self.server.gateway.writeHtmlVoltTuple()
         html += '</body></html>'
         self.writeHtml(html)
-
+    def writeSampleVolt(self, qs):
+        self.writeHead(200) # 200: 성공
+        qdict = parse.parse_qs(qs)
+        nCount = int(qdict['count'][0])
+        delay = float(qdict['delay'][0])
+        self.server.gateway.clearVoltTuple()
+        nTime = time.time()
+        self.server.gateway.sampleVoltTuple(nCount, delay)
+        self.server.gateway.saveVoltTupleIntoTable()
+        nMeasCount = self.server.gateway.countVoltTable()
+        self.server.gateway.loadVoltTupleFromTable()
+        html = '<html><head>'
+        html += '<meta http-equiv="content-type" content="text/html" charset="UTF-8">'
+        html += '<title>전압 여러 번 측정</title>'
+        html += '</head><body>'
+        html += f'<div><h5>측정 시간: {time.ctime(nTime)}</h5></div>'
+        html += f'<div><p>전압을 {nCount}번 샘플링했습니다.</p>'
+        html += f'<p>현재까지 {nMeasCount}번 측정했습니다.</div>'
+        html += self.server.gateway.writeHtmlVoltTuple()
+        html += '</body></html>'
+        self.writeHtml(html)
+        
 
 
     
